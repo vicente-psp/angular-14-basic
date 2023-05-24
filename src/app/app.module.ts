86f0658b1d63b341e562e1d4933def54aa6b8ee4
Vicente Silva <vicente.pspenha@gmail.com>
2023-05-24 18:30:01 -0300

    feat: envent binding examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Example01InterpolationComponent } from './example01-interpolation/example01-interpolation.component';
+import { Example03EventBindingComponent } from './example03-event-binding/example03-event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Example01InterpolationComponent
+    Example01InterpolationComponent,
+    Example03EventBindingComponent
   ],
   imports: [
     BrowserModule
