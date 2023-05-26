0113cc7958f8eae5aaef66098f18c92347b242ba
Vicente Silva <vicente.pspenha@gmail.com>
2023-05-26 13:06:30 -0300

    feat: two way data binding examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Example01InterpolationComponent } from './example01-interpolation/example01-interpolation.component';
+import { Example04TwoWayDataBindingComponent } from './example04-two-way-data-binding/example04-two-way-data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Example01InterpolationComponent
+    Example01InterpolationComponent,
+    Example04TwoWayDataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
