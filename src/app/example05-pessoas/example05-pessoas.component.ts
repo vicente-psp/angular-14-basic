2d1ebe620a7c9faa4e6d23d1b7ff18273bca3c2f
Vicente Silva <vicente.pspenha@gmail.com>
2023-06-01 22:44:56 -0300

    refactor: update form for reative forms

--- src/app/example05-pessoas/example05-pessoas.component.ts
+++ src/app/example05-pessoas/example05-pessoas.component.ts
@@ -1,24 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-
-interface Pessoa {
-  nome: string;
-  dataNascimento: string;
-  telefone: string;
-  rua?: string;
-  numero?: string;
-  bairro?: string;
-  email: string;
-}
-
-const factoryPessoa = (): Pessoa => {
-  return {
-    nome: '',
-    dataNascimento: '',
-    telefone: '',
-    email: ''
-  }
-}
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-example05-pessoas',
@@ -27,44 +8,25 @@ const factoryPessoa = (): Pessoa => {
 })
 export class Example05PessoasComponent {
 
-  mensagem = 'formulário inválido';
   titulo = 'Cadastro de pessoas';
-  pessoa: Pessoa = factoryPessoa();
-  controlTelefone = new FormControl('', [Validators.required, Validators.pattern(/^[(]\d{2}[)]\d{4,5}[-]\d{4}/gi)]);
+  formGroupPessoa = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    dataNascimento: new FormControl('', Validators.required),
+    telefone: new FormControl('', [Validators.required, Validators.pattern(/^[(]\d{2}[)]\d{4,5}[-]\d{4}/gi)]),
+    rua: new FormControl(''),
+    numero: new FormControl<number | null>(null),
+    bairro: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
 
   salvar(): void {
-    console.log('pessoa => ', this.pessoa);
-    if (this.isValidPessoa()) {
-      console.log('salvar pessoa');
+    if (this.formGroupPessoa.valid) {
+      console.log('salvar pessoa => ', this.formGroupPessoa.value);
     }
   }
 
   limpar(): void {
-    console.log('controlTelefone.value => ', this.controlTelefone.value);
-    console.log('controlTelefone.valid => ', this.controlTelefone.valid);
-    this.pessoa = factoryPessoa();
+    this.formGroupPessoa.reset();
   }
 
-  isValidPessoa(): boolean {
-    if (!this.isValidNome() || !this.pessoa.dataNascimento
-      || !this.isValidTelefone() || !this.isValidEmail()) {
-      this.mensagem = 'formulário inválido';
-      return false;
-    }
-    this.mensagem = 'formulário válido';
-    return true;
-  }
-
-  isValidNome(): boolean {
-    return !(!this.pessoa.nome || this.pessoa.nome.trim().length < 2 || this.pessoa.nome.trim().length > 15);
-  }
-
-  isValidTelefone(): boolean {
-    const regex = /^[(]\d{2}[)]\d{4,5}[-]\d{4}/gi
-    return !(!this.pessoa.telefone || !regex.test(this.pessoa.telefone));
-  }
-
-  isValidEmail(): boolean {
-    return !(!this.pessoa.email || this.pessoa.email.trim().length < 6);
-  }
 }
