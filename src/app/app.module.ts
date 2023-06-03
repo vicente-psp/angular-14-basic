85780488a7ac19581fb1f7c78dc23c78c1f4bbe8
Vicente Silva <vicente.pspenha@gmail.com>
2023-06-03 10:21:20 -0300

    feat: implements ngif directive examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Example01InterpolationComponent } from './example01-interpolation/example01-interpolation.component';
+import { NgifComponent } from './example06-directives/ngif/ngif.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Example01InterpolationComponent
+    Example01InterpolationComponent,
+    NgifComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
