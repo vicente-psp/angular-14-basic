1deb7694c162fcbd8ba82cc7e4ec9ae863a0aa79
Vicente Silva <vicente.pspenha@gmail.com>
2023-06-06 22:54:14 -0300

    feat: implements ngswitch directive examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { Example01InterpolationComponent } from './example01-interpolation/example01-interpolation.component';
 import { NgifComponent } from './example06-directives/ngif/ngif.component';
 import { NgforComponent } from './example06-directives/ngfor/ngfor.component';
+import { NgswitchComponent } from './example06-directives/ngswitch/ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Example01InterpolationComponent,
     NgifComponent,
-    NgforComponent
+    NgforComponent,
+    NgswitchComponent
   ],
   imports: [
     BrowserModule,
