d6b40e47d8a5ec5c7fda684f2565bdaa1698dd36
Vicente Silva <vicente.pspenha@gmail.com>
2023-06-08 15:43:45 -0300

    refactor: update packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Example01InterpolationComponent } from './example01-interpolation/example01-interpolation.component';
-import { NgifComponent } from './example06-directives/ngif/ngif.component';
-import { NgforComponent } from './example06-directives/ngfor/ngfor.component';
-import { NgswitchComponent } from './example06-directives/ngswitch/ngswitch.component';
+import { NgifComponent } from './example06-directives/structural-directives/ngif/ngif.component';
+import { NgforComponent } from './example06-directives/structural-directives/ngfor/ngfor.component';
+import { NgswitchComponent } from './example06-directives/structural-directives/ngswitch/ngswitch.component';
 
 @NgModule({
   declarations: [
