8d350517d7fa76c4106e090fc526bdd46564e127
Vicente Silva <vicente.pspenha@gmail.com>
2023-06-11 19:15:46 -0300

    feat: display and hide elements HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-14-basic';
+
+  appExample01Interpolation = true;
+  appNgif = false;
+  appNgfor = false;
+  appNgswitch = false;
+  appAttributeDirectives = false;
+  appReview = false;
+
+  /**
+   * vai selecionar o app vindo do select
+   * @param $event
+   */
+  selectApp(event: any): void {
+    this.appExample01Interpolation = event.srcElement.value === 'appExample01Interpolation';
+    this.appNgif = event.srcElement.value === 'appNgif';
+    this.appNgfor = event.srcElement.value === 'appNgfor';
+    this.appNgswitch = event.srcElement.value === 'appNgswitch';
+    this.appAttributeDirectives = event.srcElement.value === 'appAttributeDirectives';
+    this.appReview = event.srcElement.value === 'appReview';
+  }
 }
