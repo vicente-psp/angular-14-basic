467fcab53613f12560fda73e4b8d00d26195e11f
Vicente Silva <vicente.pspenha@gmail.com>
2023-10-15 21:22:38 -0300

    feat: implements pipes examples

---
+++ src/app/example10-pipes/example10-pipes.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { Example10PipesPipe } from './example10-pipes.pipe';
+
+describe('Example10PipesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new Example10PipesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
